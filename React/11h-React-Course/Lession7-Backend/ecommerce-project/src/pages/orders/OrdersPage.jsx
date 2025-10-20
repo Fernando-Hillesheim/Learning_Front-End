94c10c31a077a22ded21f6208be7c4f00410ba70
Fernando-Hillesheim <fernandohillfmh@gmail.com>
2025-10-20 18:06:10 -0300

    on going

--- React/11h-React-Course/Lession7-Backend/ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ React/11h-React-Course/Lession7-Backend/ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -1,6 +1,19 @@
+import axios from "axios";
+import { useState, useEffect, Fragment } from "react";
 import { Header } from "../../components/Header/Header";
 import "./OrdersPage.css";
+import dayjs from "dayjs";
+import { formatMoney } from "../../utils/money";
+
 export function OrdersPage({ cart }) {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get("api/orders?expand=products").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+
   return (
     <>
       <title>Orders</title>
@@ -10,6 +23,69 @@ export function OrdersPage({ cart }) {
         <div className="page-title">Your Orders</div>
 
         <div className="orders-grid">
+          {orders.map((order) => {
+            return (
+              <div key={order.id} className="order-container">
+                <div className="order-header">
+                  <div className="order-header-left-section">
+                    <div className="order-date">
+                      <div className="order-header-label">Order Placed:</div>
+                      <div>{dayjs(order.orderTimeMs).format("MMMM D")} </div>
+                    </div>
+                    <div className="order-total">
+                      <div className="order-header-label">Total:</div>
+                      <div>{formatMoney(order.totalCostCents)}</div>
+                    </div>
+                  </div>
+
+                  <div className="order-header-right-section">
+                    <div className="order-header-label">Order ID:</div>
+                    <div>{order.id}</div>
+                  </div>
+                </div>
+
+                <div className="order-details-grid">
+                  {order.products.map((product) => {
+                    return (
+                      // 7:35:21 -- video
+                      <Fragment key={product.id}>
+                        <div className="product-image-container">
+                          <img src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                        </div>
+
+                        <div className="product-details">
+                          <div className="product-name">
+                            Black and Gray Athletic Cotton Socks - 6 Pairs
+                          </div>
+                          <div className="product-delivery-date">
+                            Arriving on: August 15
+                          </div>
+                          <div className="product-quantity">Quantity: 1</div>
+                          <button className="buy-again-button button-primary">
+                            <img
+                              className="buy-again-icon"
+                              src="images/icons/buy-again.png"
+                            />
+                            <span className="buy-again-message">
+                              Add to Cart
+                            </span>
+                          </button>
+                        </div>
+
+                        <div className="product-actions">
+                          <a href="/tracking">
+                            <button className="track-package-button button-secondary">
+                              Track package
+                            </button>
+                          </a>
+                        </div>
+                      </Fragment>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
           <div className="order-container">
             <div className="order-header">
               <div className="order-header-left-section">
